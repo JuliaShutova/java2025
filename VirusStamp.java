8d52cb51b2af7848adfa7ce61faf21fa32ac51e4
Julie <yulia.shutova05@gmail.com>
2025-09-26 15:25:26 +0300

    Основы классов и объектов

--- VirusStamp.java
+++ VirusStamp.java
@@ -1,5 +1,4 @@
 abstract class VirusStamp {
-    // базовый класс
     protected String name;
     protected double infectiousness;
 
@@ -19,49 +18,24 @@ abstract class VirusStamp {
     }
 }
 
-
 class InfluenzaVirusStamp extends VirusStamp {
-    // наследуемый класс, ребёнок 1
     public InfluenzaVirusStamp() {
         super("Influenza", 0.7);
     }
 
     @Override
     public void mutate() {
-        infectiousness *= 1.1; // Грипп мутирует, увеличивая заразность на 10%
-        System.out.println(name + " has mutated! New infectiousness: " + infectiousness);
+        infectiousness *= 1.1;
     }
 }
 
-
 class CovidVirusStamp extends VirusStamp {
-    // ребёнок 2
     public CovidVirusStamp() {
         super("COVID-19", 0.9);
     }
 
     @Override
     public void mutate() {
-        infectiousness *= 1.2; // COVID мутирует более агрессивно
-        System.out.println(name + " has mutated into a new variant! New infectiousness: " + infectiousness);
-    }
-}
-
-
-public class VirusSimulation {
-    public static void main(String[] args) {
-        // создаем массив штампов вирусов (базового типа)
-        VirusStamp[] virusStamps = {
-            new InfluenzaVirusStamp(),
-            new CovidVirusStamp()
-        };
-
-        // симулируем мутацию для каждого вируса
-        
-        for (VirusStamp virus : virusStamps) {
-            System.out.println("Before mutation: " + virus.getDescription());
-            virus.mutate(); 
-            System.out.println("-------------------");
-        }
+        infectiousness *= 1.2;
     }
 }
