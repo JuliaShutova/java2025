3ad3e6dfcb20bc4885e5a74915ff47cb42ecfe24
Julie <yulia.shutova05@gmail.com>
2025-11-13 17:30:25 +0300

    Add files via upload

--- VariableExample.java
+++ VariableExample.java
@@ -1,11 +1,11 @@
 public class VariableExample {
     public static void main(String[] args) {
         int a = 1;
-        int b; // объявляем b в той же области видимости, что и a
+        int b; 
         {
-            b = 2; // инициализируем b внутри блока
+            b = 2; 
         }
-        int c = a + b; // теперь работает, c = 3
-        System.out.println("c = " + c); // выведет: c = 3
+        int c = a + b; 
+        System.out.println("c = " + c); 
     }
 }
