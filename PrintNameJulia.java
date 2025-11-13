6c13be3eb65f7326d863841b9cabb772cd119d37
Julie <yulia.shutova05@gmail.com>
2025-11-13 17:23:47 +0300

    Add files via upload

---
+++ PrintNameJulia.java
@@ -0,0 +1,11 @@
+public class PrintNameJulia {
+    public static void main(String[] args) {
+        char letter1 = '\u004A';
+        char letter2 = '\u0075';
+        char letter3 = '\u006C';
+        char letter4 = '\u0069';
+        char letter5 = '\u0061';
+        
+        System.out.println("" + letter1 + letter2 + letter3 + letter4 + letter5);
+    }
+}
