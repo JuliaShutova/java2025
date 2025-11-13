30bd74bc20c1363d32ca2d533f6c07d37f9258ca
Julie <yulia.shutova05@gmail.com>
2025-11-13 18:39:21 +0300

    Add files via upload

---
+++ BreakContinueExample.java
@@ -0,0 +1,14 @@
+public class BreakContinueExample {
+    public static void main(String[] args) {
+        
+        for (int i = 1; i <= 10; i++) {
+            if (i == 4) {
+                continue;
+            }
+            if (i == 7) {
+                break;
+            }
+            System.out.println("i = " + i);
+        }
+    }
+}
