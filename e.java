db056217022b2334a22bc5ae57dcb52d00c220de
Julie <yulia.shutova05@gmail.com>
2025-11-14 16:55:00 +0300

    Add files via upload

---
+++ e.java
@@ -0,0 +1,18 @@
+class MathOperations {
+    
+    public int calculate(int a, int b) {
+        return a + b;
+    }
+    
+    public double calculate(double a, double b) {
+        return a * b;
+    }
+    
+    public int calculate(int a, int b, int c) {
+        return a - b - c;
+    }
+    
+    public String calculate(String a, String b) {
+        return a + " и " + b;
+    }
+}
